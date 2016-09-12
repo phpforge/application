6c874e8f1264bb9f19a222ec31588dd3431b9b42
Benjamin C. Tehan <benjamin.tehan@devforge.io>
2016-09-12 16:18:28 +1000

    Patch base uri

--- src/Forge/Application.php
+++ src/Forge/Application.php
@@ -104,7 +104,7 @@ class Application extends Base {
 			foreach ($module->getRoutes() as $route) {
 				if ($route->getRequestMethod() === $requestMethod) {
 					foreach ($route->getUrls() as $url) {
-						if (strtolower($url) === strtolower(!empty($uri) ? $uri : '/')) {
+						if (preg_replace('/\/$/', '', strtolower(BASE_URI . $url)) === strtolower(!empty($uri) ? $uri : '')) {
 
 							$request = new Request();
 							$request->setRoute($route)
@@ -129,9 +129,9 @@ class Application extends Base {
 			}
 		}
 
-		$nextUri = preg_replace('/\/[^\/]+$/', '', $requestUri);
-		if (!empty($nextUri)) {
-			return $this->buildRequest($originalUri, $nextUri, $requestMethod);
+		if ($requestUri !== '/') {
+			$nextUri = preg_replace('/\/[^\/]+$/', '', $requestUri);
+			return $this->buildRequest($originalUri, (!empty($nextUri) ? $nextUri : '/'), $requestMethod);
 		}
 
 		return null;
